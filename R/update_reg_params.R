f9659b667314ffd04cd378d729821a0ec038546e
Eduardo Vasquez <eduardovasqueztapia@gmail.com>
2026-06-02 14:07:50 -0400

    Addition of covariates - regression parameter updates changed

--- R/update_reg_params.R
+++ R/update_reg_params.R
@@ -1,72 +1,90 @@
-eta_star <- function(Eta, k) {
-  m <- k * (k + 1L) / 2L
-  Eta_star <- matrix(0, nrow(Eta), m)
-  idx <- 1L
-  for (l in seq_len(k)) {
-    for (h in seq_len(l)) {
-      Eta_star[, idx] <- if (h == l) Eta[, h]^2 else 2 * Eta[, h] * Eta[, l]
-      idx <- idx + 1L
-    }
-  }
-  Eta_star
-}
-
-vech_to_sym <- function(omega_vec, k) {
-  Omega <- matrix(0, k, k)
-  idx <- 1L
-  for (l in seq_len(k)) {
-    for (h in seq_len(l)) {
-      Omega[h, l] <- omega_vec[idx]
-      Omega[l, h] <- omega_vec[idx]
-      idx <- idx + 1L
-    }
-  }
-  Omega
-}
-
 update_reg_params <- function(
   reg_params,
   y,
+  Z,
   Eta,
   beta_var,
   Omega_var,
+  alpha_var,
+  Delta_var,
   shape_sigmay,
+  dup_matrix,
   n,
+  q,
   k
 ) {
   beta <- reg_params$beta
   sigma2_y <- reg_params$sigma2_y
+
   Omega <- reg_params$Omega
   inter <- !is.null(Omega)
-  quad_term <- if (inter) rowSums((Eta %*% Omega) * Eta) else 0
+  quad_term_Omega <- if (inter) rowSums((Eta %*% Omega) * Eta) else 0
+
+  alpha <- reg_params$alpha
+  covs <- !is.null(alpha)
+  term_alpha <- if (covs) Z %*% alpha else 0
+
+  Delta <- reg_params$Delta
+  covs_inter <- !is.null(Delta)
+  quad_term_Delta <- if (covs_inter) rowSums((Eta %*% Delta) * Z) else 0
 
   # 1. Update beta
-  resid_beta <- y - quad_term
+  r_beta <- y - quad_term_Omega - term_alpha - quad_term_Delta
   V_beta <- solve(crossprod(Eta) / sigma2_y + diag(1 / beta_var, nrow = k))
-  mu_beta <- V_beta %*% (crossprod(Eta, resid_beta) / sigma2_y)
+  mu_beta <- V_beta %*% (crossprod(Eta, r_beta) / sigma2_y)
   beta <- as.numeric(mvnfast::rmvn(1, mu_beta, V_beta))
+  term_beta <- Eta %*% beta
 
-  # 2. Update Omega
-  if (inter) {
-    Eta_star <- eta_star(Eta, k) # n x k(k+1)/2
-    resid_Omega <- y - Eta %*% beta
-    m <- k * (k + 1L) / 2L
-    P_Omega <- crossprod(Eta_star) / sigma2_y + diag(1 / Omega_var, nrow = m)
-    V_Omega <- solve(P_Omega)
-    mu_Omega <- V_Omega %*% (crossprod(Eta_star, resid_Omega) / sigma2_y)
-    omega_vec <- as.numeric(mvnfast::rmvn(1, mu_Omega, V_Omega))
-    Omega <- vech_to_sym(omega_vec, k)
+  # 2. Update alpha
+  if (covs) {
+    r_alpha <- y - term_beta - quad_term_Omega - quad_term_Delta
+    V_alpha <- solve(crossprod(Z) / sigma2_y + diag(1 / alpha_var, nrow = q))
+    mu_alpha <- V_alpha %*% (crossprod(Z, r_alpha) / sigma2_y)
+    alpha <- as.numeric(mvnfast::rmvn(1, mu_alpha, V_alpha))
+    term_alpha <- Z %*% alpha
   }
 
-  # 3. Update sigma2_y
-  resid_all <- y - Eta %*% beta - quad_term
-  rate_sigmay <- 0.5 + 0.5 * as.numeric(crossprod(resid_all))
+  # 3. Update Delta
+  if (covs_inter) {
+    r_Delta <- y - term_beta - term_alpha - quad_term_Omega
+    W <- t(sapply(1:n, \(i) t(kronecker(Z[i, ], Eta[i, ]))))
+    V_Delta <- solve(
+      crossprod(W) / sigma2_y + diag(1 / Delta_var, nrow = k * q)
+    )
+    mu_Delta <- V_Delta %*% (crossprod(W, r_Delta) / sigma2_y)
+    Delta <- matrix(as.numeric(mvnfast::rmvn(1, mu_Delta, V_Delta)), k, q)
+    quad_term_Delta <- rowSums((Eta %*% Delta) * Z)
+  }
+
+  # 4. Update Omega
+  if (inter) {
+    r_Omega <- y - term_beta - term_alpha - quad_term_Delta
+    H <- t(sapply(1:n, \(i) t(kronecker(Eta[i, ], Eta[i, ]))))
+    HD <- H %*% dup_matrix
+    V_Omega <- solve(
+      crossprod(HD) / sigma2_y + diag(1 / Omega_var, nrow = k * (k + 1) / 2)
+    )
+    mu_Omega <- V_Omega %*% (crossprod(HD, r_Omega) / sigma2_y)
+    vech_Omega <- as.numeric(mvnfast::rmvn(1, mu_Omega, V_Omega))
+    Omega <- vech2sym(vech_Omega)
+    quad_term_Omega <- rowSums((Eta %*% Omega) * Eta)
+  }
+
+  # 5. Update sigma2_y
+  r <- y - term_beta - quad_term_Omega - term_alpha - quad_term_Delta
+  rate_sigmay <- 0.5 + 0.5 * as.numeric(crossprod(r))
   sigma2_y <- 1 / stats::rgamma(1, shape_sigmay, rate_sigmay)
 
-  # 4. Return
+  # 6. Return
   out <- list(sigma2_y = sigma2_y, beta = beta)
   if (inter) {
     out$Omega <- Omega
+  }
+  if (covs) {
+    out$alpha <- alpha
+  }
+  if (covs_inter) {
+    out$Delta <- Delta
   }
   out
 }
--- R/utils.R
+++ R/utils.R
@@ -13,3 +13,31 @@ mat_long <- function(mat, label) {
     panel = label
   )
 }
+
+## Half-vectorization and inverse, taken from OpenMx
+vech <- function(X) {
+  X[lower.tri(X, diag = TRUE)]
+}
+
+vech2sym <- function(X) {
+  if (is.matrix(X)) {
+    if (nrow(X) > 1 && ncol(X) > 1) {
+      stop("Input to the full vech2full must be a (1 x n) or (n x 1) matrix.")
+    }
+    dimension <- max(dim(X))
+  } else if (is.vector(X)) {
+    dimension <- length(X)
+  } else {
+    stop("Input to the function vech2full must be either a matrix or a vector.")
+  }
+  k <- sqrt(2 * dimension + 0.25) - 0.5
+  ret <- matrix(0, nrow = k, ncol = k)
+  if (nrow(ret) != k) {
+    stop(
+      "Incorrect number of elements in vector to construct a matrix from a half-vectorization."
+    )
+  }
+  ret[lower.tri(ret, diag = TRUE)] <- as.vector(X)
+  ret[upper.tri(ret)] <- t(ret)[upper.tri(ret)]
+  return(ret)
+}
